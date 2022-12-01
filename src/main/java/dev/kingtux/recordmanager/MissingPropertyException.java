ee0e250fb37ac00fce13c1611b9f1b7c281bdd23
Wyatt Herkamp <wherkamp@kingtux.me>
2022-12-01 14:16:56 -0500

    Revert "Init"
    This reverts commit 6737d38c7a8abcb6c27194cd3d6765ee1283757f.

--- src/main/java/dev/kingtux/recordmanager/MissingPropertyException.java
+++
@@ -1,7 +0,0 @@
-package dev.kingtux.recordmanager;
-
-public class MissingPropertyException extends Exception{
-    public MissingPropertyException(String property) {
-        super("Missing property " + property);
-    }
-}
